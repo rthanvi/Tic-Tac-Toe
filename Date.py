cb69ca911d836d065706e4a59caa3ca3a5b886f6
rthanvi <98587607+rthanvi@users.noreply.github.com>
2022-05-01 16:49:32 +0530

    Add files via upload

---
+++ Date.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+dt=datetime(2013,4,5,2,30,45)
+print(dt.hour)
+print(dt.minute)
+print(dt.second)
+print(dt.year)
+print(dt.month)
+print(dt.date)
+print(dt.strftime("%d-%m-%Y|%H:%M:%S"))
